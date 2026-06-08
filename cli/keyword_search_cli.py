8fcd57ab001dfdb311f19d296f56774aed86e2af
Cyberis <christopher.bortz@gmail.com>
2026-06-08 02:20:43 -0400

    Add Naive Keyword Search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -13,9 +14,21 @@ def main() -> None:
 
     match args.command:
         case "search":
-             print(f"Searching for: {args.query}")
+             search_movies(args.query)
         case _:
             parser.print_help()
+            
+def search_movies(query: str) -> None:
+    # Placeholder for the actual search logic
+    print(f"Searching for: {query}\n")
+    with open("data/movies.json", "r") as f:    
+        movies = json.load(f)
+        results = [movie for movie in movies["movies"] if query.lower() in movie["title"].lower()]
+        print(f"Found {len(results)} results:") # This is an optional line no in the lesson specifications
+        for i, movie in enumerate(results, start=1):
+            print(f"{i}. {movie['title']}")
+            if i >= 5:  # Limit to top 5 results
+                break
 
 if __name__ == "__main__":
     main()
