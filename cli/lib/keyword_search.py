2358e1ce7683187bbd99b0f892d3de12f68d48a8
Cyberis <christopher.bortz@gmail.com>
2026-06-12 14:10:07 -0400

    Add Steming For Both Query and Title Tokens

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,6 +1,8 @@
 from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
 import string
+from nltk.stem import PorterStemmer
 
+stemmer = PorterStemmer()
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
@@ -24,6 +26,8 @@ def preprocess_text(text: str, stopwords: set[str]) -> set[str]:
     tokens = set(text.split())
     # Step four, remove stopwords
     tokens = tokens - stopwords
+    # Step five, apply stemming
+    tokens = set(stemmer.stem(token) for token in tokens)
     return tokens
 
 def found(query_tokens: set[str], title_tokens: set[str]) -> bool:
